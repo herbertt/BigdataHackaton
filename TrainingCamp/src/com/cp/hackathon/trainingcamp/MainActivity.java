0e57da118839e528cd82c3ab61e7ae1985e40638
hbmd <hbmd@cin.ufpe.br>
2015-07-24 02:52:47 -0300

    conneting muse

--- TrainingCamp/src/com/cp/hackathon/trainingcamp/MainActivity.java
+++ TrainingCamp/src/com/cp/hackathon/trainingcamp/MainActivity.java
@@ -1,15 +1,40 @@
 package com.cp.hackathon.trainingcamp;
 
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.trainingcamp.R;
+import com.interaxon.libmuse.ConnectionState;
+import com.interaxon.libmuse.Eeg;
+import com.interaxon.libmuse.LibMuseVersion;
+import com.interaxon.libmuse.Muse;
+import com.interaxon.libmuse.MuseArtifactPacket;
+import com.interaxon.libmuse.MuseConnectionListener;
+import com.interaxon.libmuse.MuseConnectionPacket;
+import com.interaxon.libmuse.MuseDataListener;
+import com.interaxon.libmuse.MuseDataPacket;
+import com.interaxon.libmuse.MuseDataPacketType;
+import com.interaxon.libmuse.MuseFileFactory;
+import com.interaxon.libmuse.MuseFileWriter;
+import com.interaxon.libmuse.MuseManager;
+import com.interaxon.libmuse.MusePreset;
+import com.interaxon.libmuse.MuseVersion;
+
+
 
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -23,8 +48,22 @@ public class MainActivity extends Activity {
 	private LocationManager mlocManager;
 	private LocationListener mlocListener;
 	private boolean isGPSEnabled = true;
+    private Muse muse = null;
+    private ConnectionListener connectionListener = null;
+    private DataListener dataListener = null;
+    private boolean dataTransmission = true;
+    private MuseFileWriter fileWriter = null;
+		    
+		    
 	
-    @Override
+    public MainActivity() {
+    	  WeakReference<Activity> weakActivity = new WeakReference<Activity>(this);
+          connectionListener = new ConnectionListener(weakActivity);
+          dataListener = new DataListener(weakActivity);
+	}
+
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -33,7 +72,13 @@ public class MainActivity extends Activity {
         //Necessário para usar Runable na activity?
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-       
+        
+        File dir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        fileWriter = MuseFileFactory.getMuseFileWriter(new File(dir, "new_muse_file.muse"));
+	    Log.i("Muse Headband", "libmuse version=" + LibMuseVersion.SDK_VERSION);
+	    fileWriter.addAnnotationString(1, "MainActivity onCreate");
+	    dataListener.setFileWriter(fileWriter);
+        connectMuse();
     }
 
 
@@ -70,6 +115,24 @@ public class MainActivity extends Activity {
 				}
 				
 			}
+		}
+		
+		private void publishProgress(String param) {
+
+			final String resultado = param;
+
+			runOnUiThread(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+					//	updateResults(resultado);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
 		}
     }
     
@@ -163,6 +226,9 @@ public class MainActivity extends Activity {
 
 		@Override 
 		public void onProviderDisabled(String provider) { 
+			
+			 muse.disconnect(true);
+			
 		} 
 
 		@Override 
@@ -178,8 +244,180 @@ public class MainActivity extends Activity {
 
 	public void parallelTasks() {
 		// TODO Auto-generated method stub
-		Toast.makeText(getApplicationContext(), "[TP] Your location is - \nLat: " + latitudeString + "\nLong: " + longitudeString, Toast.LENGTH_LONG).show();
+		//Toast.makeText(getApplicationContext(), "[TP] Your location is - \nLat: " + latitudeString + "\nLong: " + longitudeString, Toast.LENGTH_LONG).show();
 	} 
 	
+	
+	
+	/**
+	 * conection muse 
+	 */
+	
+	/**
+     * Connection listener updates UI with new connection status and logs it.
+     */
+    class ConnectionListener extends MuseConnectionListener {
+
+        final WeakReference<Activity> activityRef;
+
+        ConnectionListener(final WeakReference<Activity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseConnectionPacket(MuseConnectionPacket p) {
+            final ConnectionState current = p.getCurrentConnectionState();
+            final String status = p.getPreviousConnectionState().toString() +
+                         " -> " + current;
+            final String full = "Muse " + p.getSource().getMacAddress() +
+                                " " + status;
+            Log.i("Muse Headband", full);
+            Activity activity = activityRef.get();
+            // UI thread is used here only because we need to update
+            // TextView values. You don't have to use another thread, unless
+            // you want to run disconnect() or connect() from connection packet
+            // handler. In this case creating another thread is required.
+            if (activity != null) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView statusText =
+                                (TextView) findViewById(R.id.con_status);
+                        statusText.setText(status);
+                        TextView museVersionText =
+                                (TextView) findViewById(R.id.version);
+                        if (current == ConnectionState.CONNECTED) {
+                            MuseVersion museVersion = muse.getMuseVersion();
+                            String version = museVersion.getFirmwareType() +
+                                 " - " + museVersion.getFirmwareVersion() +
+                                 " - " + Integer.toString(
+                                    museVersion.getProtocolVersion());
+                            museVersionText.setText(version);
+                        } else {
+                            museVersionText.setText(R.string.undefined);
+                        }
+                    }
+                });
+            }
+        }
+    }
+    
+    /**
+     * Data listener will be registered to listen for: Accelerometer,
+     * Eeg and Relative Alpha bandpower packets. In all cases we will
+     * update UI with new values.
+     * We also will log message if Artifact packets contains "blink" flag.
+     * DataListener methods will be called from execution thread. If you are
+     * implementing "serious" processing algorithms inside those listeners,
+     * consider to create another thread.
+     */
+    class DataListener extends MuseDataListener {
+
+        final WeakReference<Activity> activityRef;
+        private MuseFileWriter fileWriter;
+
+        DataListener(final WeakReference<Activity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseDataPacket(MuseDataPacket p) {
+            switch (p.getPacketType()) {
+                case ALPHA_RELATIVE:
+                    updateAlphaRelative(p.getValues());
+                    break;
+                case BATTERY:
+                    fileWriter.addDataPacket(1, p);
+                    // It's library client responsibility to flush the buffer,
+                    // otherwise you may get memory overflow. 
+                    if (fileWriter.getBufferedMessagesSize() > 8096)
+                        fileWriter.flush();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void receiveMuseArtifactPacket(MuseArtifactPacket p) {
+            if (p.getHeadbandOn() && p.getBlink()) {
+                Log.i("Artifacts", "blink");
+            }
+        }
+
+        private void updateAlphaRelative(final ArrayList<Double> data) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                         TextView elem1 = (TextView) findViewById(R.id.elem1);
+                         TextView elem2 = (TextView) findViewById(R.id.elem2);
+                         TextView elem3 = (TextView) findViewById(R.id.elem3);
+                         TextView elem4 = (TextView) findViewById(R.id.elem4);
+                         elem1.setText(String.format(
+                            "%6.2f", data.get(Eeg.TP9.ordinal())));
+                         elem2.setText(String.format(
+                            "%6.2f", data.get(Eeg.FP1.ordinal())));
+                         elem3.setText(String.format(
+                            "%6.2f", data.get(Eeg.FP2.ordinal())));
+                         elem4.setText(String.format(
+                            "%6.2f", data.get(Eeg.TP10.ordinal())));
+                    }
+                });
+            }
+        }
+
+        public void setFileWriter(MuseFileWriter fileWriter) {
+            this.fileWriter  = fileWriter;
+        }
+    }
+    
+    public void connectMuse() {
+    	 MuseManager.refreshPairedMuses();
+    	 List<Muse> pairedMuses = MuseManager.getPairedMuses();
+         if (pairedMuses.size() < 1) {
+             Log.w("Muse Headband", "There is nothing to connect to");
+         }
+         else {
+             muse = pairedMuses.get(0);
+             ConnectionState state = muse.getConnectionState();
+             if (state == ConnectionState.CONNECTED ||
+                 state == ConnectionState.CONNECTING) {
+                 Log.w("Muse Headband",
+                 "doesn't make sense to connect second time to the same muse");
+                 return;
+             }
+             configureLibrary();
+             fileWriter.open();
+             fileWriter.addAnnotationString(1, "Connected");
+             /**
+              * In most cases libmuse native library takes care about
+              * exceptions and recovery mechanism, but native code still
+              * may throw in some unexpected situations (like bad bluetooth
+              * connection). Print all exceptions here.
+              */
+             try {
+                 muse.runAsynchronously();
+             } catch (Exception e) {
+                 Log.e("Muse Headband", e.toString());
+             }
+         }
+	}
+    private void configureLibrary() {
+        muse.registerConnectionListener(connectionListener);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.ACCELEROMETER);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.EEG);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.ALPHA_RELATIVE);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.ARTIFACTS);
+        muse.registerDataListener(dataListener,
+                                  MuseDataPacketType.BATTERY);
+        muse.setPreset(MusePreset.PRESET_14);
+        muse.enableDataTransmission(dataTransmission);
+    }
 
 }
